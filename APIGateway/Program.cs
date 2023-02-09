191bab676745664cb0a87c240521036d6a2d4cc6
badea741 <mohammedahmedbadee@gmail.com>
2023-02-09 17:39:35 +0200

    modified program.cs

--- APIGateway/Program.cs
+++ APIGateway/Program.cs
@@ -9,8 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.Development.json", optional: false, reloadOnChange: true)
+var env = builder.Environment.EnvironmentName;
+builder.Configuration
+    .AddJsonFile($"ocelot.{env}.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
 builder.Services.AddOcelot(builder.Configuration);
 
