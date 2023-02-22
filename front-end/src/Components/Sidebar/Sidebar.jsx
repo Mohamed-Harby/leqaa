c32988fd614a38654631a4ff27b1543e14835c6f
mohamed20medhat <egailmohamed2018@gmail.com>
2023-02-22 19:58:06 +0200

    toggle mic

--- front-end/src/Components/Sidebar/Sidebar.jsx
+++ front-end/src/Components/Sidebar/Sidebar.jsx
@@ -13,7 +13,7 @@ function Sidebar() {
   return (
     <div className={closeSidebar ? "sidebar sidebarResponsive" : "sidebar"}>
       <ul>
-        <li class="top">
+        <li className="top">
           <div className={closeSidebar ? "edit-control" : "control"}>
             <FaBars
               className={closeSidebar ? "showBar" : "bar"}
@@ -48,7 +48,7 @@ function Sidebar() {
           </NavLink>
         </li>
 
-        <li class="bottom">
+        <li className="bottom">
           <NavLink to="/settings">
             <BsGear />
             <span className={closeSidebar ? "hideText" : "text"}>Setting</span>
