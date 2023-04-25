e7b12ee7f1bc0a94476b763dac42e0cc82d5d00a
badea741 <mohammedahmedbadee@gmail.com>
2023-04-25 22:06:39 +0200

    Fixed object decoding algorithm

--- BusinessDomain/BusinessLogic.Application/Models/Users/UserWriteModel.cs
+++ BusinessDomain/BusinessLogic.Application/Models/Users/UserWriteModel.cs
@@ -1,7 +1,10 @@
+using BusinessLogic.Domain.Enums;
+
 namespace BusinessLogic.Application.Models.Channels;
 public record UserWriteModel(
     Guid Id,
     string Name,
     string Email,
-    string UserName
+    string UserName,
+    Gender Gender
 );
